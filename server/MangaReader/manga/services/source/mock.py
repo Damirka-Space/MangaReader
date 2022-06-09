eab18e7348ebdcc65d1608d69658b68c382380ba
Gakawarstone <pinnekerv@gmail.com>
2022-06-09 13:59:51 +0600

    feat: add mocked manga source

---
+++ server/MangaReader/manga/services/source/mock.py
@@ -0,0 +1,21 @@
+import random
+
+from .base import MangaSourceBase
+
+
+class MockedMangaSource(MangaSourceBase):
+    name = 'MockedSource'
+    url = 'https://source.mock/'
+
+    @classmethod
+    def get_chapter_url(cls, manga_name: str, volume_serial: str,
+                        chapter_serial: str) -> str:
+        return f'{cls.url}/{manga_name}/{volume_serial}/{chapter_serial}'
+
+    @classmethod
+    def get_chapter_frames_cnt(cls, chapter_url: str) -> int:
+        return random.randint(16, 50)
+
+    @classmethod
+    def get_frame_url(cls, chapter_url: str, frame_num: int) -> str:
+        return chapter_url + '/' + str(frame_num)
