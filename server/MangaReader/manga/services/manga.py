92474d96b6b765f7f9257160c6c58bad6ae335b8
Gakawarstone <pinnekerv@gmail.com>
2022-06-18 14:28:04 +0600

    chore: add labels

--- server/MangaReader/manga/services/manga.py
+++ server/MangaReader/manga/services/manga.py
@@ -1,7 +1,7 @@
 from .. import models
 
 
-class Manga:
+class Manga:  # NOTE MangaBrowser?
     @classmethod
     def get_by_name_in_source(cls, name: str, source_id: int) -> models.Manga:
         try:
@@ -9,14 +9,24 @@ class Manga:
         except models.Manga.DoesNotExist:
             return cls.__create(name=name, source_id=source_id)
 
-    @classmethod
+    @classmethod  # NOTE Should be not classmethod
     def __create(cls, name: str, source_id: int) -> models.Manga:
         manga, is_created = models.Manga.objects.get_or_create(
             name=name,
-            volume_cnt=0,
-            chapters_cnt=0,
-            url='test.com/manga'
+            volume_cnt=cls.__get_volume_cnt(),
+            chapters_cnt=cls.__get_chapter_cnt(),
+            url='test.com/manga'  # [ ] Valid url
         )
         if is_created:
             manga.source_name.add(models.MangaSource.objects.get(id=source_id))
         return manga
+
+    @classmethod
+    def __get_volume_cnt(cls) -> int:
+        # [ ]  Get from source
+        return 0
+
+    @classmethod
+    def __get_chapter_cnt(cls) -> int:
+        # [ ] Get from source
+        return 0
